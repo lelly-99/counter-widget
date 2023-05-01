9538d79d0b2a0ce438e31043b05453f4a8c1db54
progress <leswifil@gmail.com>
2023-05-01 15:08:47 +0200

    counter done

--- counterWidget.js
+++ counterWidget.js
@@ -1,5 +1,5 @@
-//referece to input number
-var inputNumber = document.querySelector(".inputNumber")
+//referece to add multiple
+var multiplyButton = document.querySelector(".multiplyButton")
 //reference to multiply number
 var multiplyNumber = document.querySelector(".multiplyNumber")
 //reference to counter
@@ -8,30 +8,32 @@ var counter = document.querySelector(".counter")
 var increaseButton = document.querySelector(".increaseButton")
 //reference to decrease button
 var decreaseButton = document.querySelector(".decreaseButton")
-//button to update
-var updateButton = document.querySelector(".updateButton")
+//button to reset
+var resetButton = document.querySelector(".resetButton")
 
 
-function counterWidget(){
-    var number = Number(inputNumber.value)
-    var multiply = Number(multiplyNumber.value)
-    if(isNaN(number) && isNaN(multiply)){
-        return;
-    }
-    var total = number * multiply
-    if(total !== 0){
-        counter.innerHTML
-    }
-}
+//event listener to increment
 increaseButton.addEventListener("click", function(){
     counter.innerHTML++
-    counterWidget()
 });
-
+//event listener to decrement
 decreaseButton.addEventListener("click", function(){
     counter.innerHTML--
-    counterWidget()
+    //counter can never be less than zero
+    if (counter.innerHTML < 0) {
+        counter.innerHTML = 0;
+      }
 });
-
+//event listener to multiply
+multiplyButton.addEventListener("click", function(){
+    const multiple = Number(multiplyNumber.value);
+    counter.innerHTML *= multiple
+});
+//event listener to reset to zero
+resetButton.addEventListener("click", () => {
+    inputNumber = 0;
+    multiplyNumber.value = "";
+    counter.innerHTML = 0;
+  });
 
 
